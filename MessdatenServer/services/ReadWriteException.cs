fcb8fb41c16d6b4ffb1e5543376e7b678ce45903
hansjuergnett <goeginett@hotmail.com>
2017-12-14 21:20:05 +0100

    Add ReadWriteException

---
+++ MessdatenServer/services/ReadWriteException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessdatenServer.services
+{
+    public class ReadWriteException : Exception
+    {
+        public ReadWriteException(string message)
+            : base(message)
+        {
+        }
+
+        public ReadWriteException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
