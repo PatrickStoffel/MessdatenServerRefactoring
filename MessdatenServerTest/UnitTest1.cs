402f9ff64a657e2fb446ad3789e91b6f3c810444
hansjuergnett <goeginett@hotmail.com>
2017-12-15 19:17:44 +0100

    Init UnitTest-Project

---
+++ MessdatenServerTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessdatenServerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
