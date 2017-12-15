0c0d193d8c2393e6cb8c6714c50c1cb4289f2eaf
hansjuergnett <goeginett@hotmail.com>
2017-12-15 19:21:59 +0100

    Init UnitTest3-Project with class DeviceListHandlerTests

---
+++ MessdatenServerTests/services/DeviceListHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using MessdatenServer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessdatenServer.services.Tests
+{
+    [TestFixture()]
+    public class DeviceListHandlerTests
+    {
+        [Test()]
+        public void GetDeviceFromDeviceListTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void UpdateDeviceInDeviceListTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void SaveNewDeviceInDeviceListTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void DeleteDeviceInDeviceListTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
