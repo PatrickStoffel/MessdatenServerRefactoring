df6497c65550f56b6ec571eb010ad7ec4e765caa
hansjuergnett <goeginett@hotmail.com>
2017-12-16 17:19:13 +0100

    Add positive tests for DeviceListHandler

--- MessdatenServerTests/services/DeviceListHandlerTests.cs
+++ MessdatenServerTests/services/DeviceListHandlerTests.cs
@@ -5,34 +5,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessdatenServer.Models;
 
 namespace MessdatenServer.services.Tests
 {
     [TestFixture()]
     public class DeviceListHandlerTests
     {
-        [Test()]
-        public void GetDeviceFromDeviceListTest()
+        private List<Device> devices = null;
+        private Device dev1 = null;
+        private Device dev2 = null;
+        private Device dev3 = null;
+
+
+        [SetUp]
+        public void Init()
         {
-            throw new NotImplementedException();
+            devices = new List<Device>();
+            dev1 = new Device("dev1", "230.25.35.41", "COM1", "A", "com-1");
+            dev2 = new Device("dev2", "230.25.35.42", "COM2", "B", "com-1");
+            dev3 = new Device("dev3", "230.25.35.43", "COM3", "C", "com-1");
+            devices.Add(dev1);
+            devices.Add(dev2);
+           
         }
 
         [Test()]
-        public void UpdateDeviceInDeviceListTest()
+        public void GetDeviceFromDeviceList_WithExistingDevideId_ReturnsExpectedId()
         {
-            throw new NotImplementedException();
+            Device device = DeviceListHandler.GetDeviceFromDeviceList(devices, "dev2");
+
+            Assert.AreEqual("dev2", device.Id);
         }
 
         [Test()]
-        public void SaveNewDeviceInDeviceListTest()
+        public void UpdateDeviceInDeviceList_WithChangedProtocol_ReturnsExpectedProtocol()
         {
-            throw new NotImplementedException();
+            dev1.Protocol = "txt";
+
+            DeviceListHandler.UpdateDeviceInDeviceList(devices, dev1);
+
+            Assert.AreEqual("txt", DeviceListHandler.GetDeviceFromDeviceList(devices, "dev1").Protocol);
         }
 
         [Test()]
-        public void DeleteDeviceInDeviceListTest()
+        public void SaveNewDeviceInDeviceList_WithNewId_IncrementListCount()
         {
-            throw new NotImplementedException();
+            int noOfDevicesBevorAddDeviceToList = devices.Count;
+
+            DeviceListHandler.SaveNewDeviceInDeviceList(devices, dev3);
+
+            Assert.AreEqual(noOfDevicesBevorAddDeviceToList + 1, devices.Count);
+        }
+
+        [Test()]
+        public void DeleteDeviceInDeviceList_WithExistingId_DecrementListCount()
+        {
+            int noOfDevicesBevorDeleteDeviceFromList = devices.Count;
+
+            DeviceListHandler.DeleteDeviceInDeviceList(devices, "dev1");
+
+            Assert.AreEqual(noOfDevicesBevorDeleteDeviceFromList - 1, devices.Count);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            devices = null;
+            dev1 = null;
+            dev2 = null;
+            dev3 = null;
         }
     }
 }
