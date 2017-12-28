acda6a2a2d336829bf0867638a38ba25faed3dc6
hansjuergnett <goeginett@hotmail.com>
2017-12-28 10:39:50 +0100

    Add class DeleteDeviceTest

---
+++ MessdatenServerGuiTest/DeleteDeviceTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace MessdatenServerGuiTest
+{
+    public class DeleteDeviceTest<TWebDriver> : DeviceTest<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        [Test]
+        public void DeleteDevice_WithExistingChoosen_RemoveDeviceFromConfig()
+        {
+
+        }
+    }
+}
